fc82100998512b8f58efab43be79c16205a7f604
mihorahul <mihorahul@gmail.com>
2026-08-03 14:26:26 +0530

    Setup Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div>
-      <h1>Recipe Book Pro</h1>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <h1 className="text-4xl font-bold text-blue-600">Recipe Book Pro</h1>
+
+        <p className="mt-3 text-gray-600">Project Started Successfully 🚀</p>
+      </div>
     </div>
   );
 }
